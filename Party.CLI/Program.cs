e8ed0fa6b2194e9f3e91b4930d0cf54553013ed1
Acid Bubbles <acidbubbles.anon@gmail.com>
2019-08-04 23:21:42 -0400

    CLI interface

--- Party.CLI/Program.cs
+++ Party.CLI/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.IO;
+using CommandLine;
+using Party.Shared;
 
 namespace Party.CLI
 {
     class Program
     {
-        static void Main(string[] args)
+        [Verb("scenes", HelpText = "Show all scenes")]
+        class ScenesOptions
         {
-            Console.WriteLine("Hello World!");
+            [Option('p', "path", Required = false, HelpText = "The Saves directory; defaults to the Saves folder under the current directory.")]
+            public string Saves { get; set; }
+        }
+
+        static int Main(string[] args)
+        {
+            return CommandLine.Parser.Default.ParseArguments<ScenesOptions>(args)
+              .MapResult(
+                (ScenesOptions opts) => RunScenes(opts),
+                errs => 1);
+        }
+
+        private static int RunScenes(ScenesOptions opts)
+        {
+            var savesDirectory = Path.GetFullPath(opts.Saves ?? Path.Combine(Environment.CurrentDirectory, "Saves"));
+            var saves = SavesScanner.Scan(savesDirectory);
+
+            Console.WriteLine("Scenes:");
+            foreach (var scene in saves.Scenes)
+            {
+                Console.WriteLine($"- {scene.Filename}");
+            }
+
+            Console.WriteLine("Scripts:");
+            foreach (var script in saves.Scripts)
+            {
+                Console.WriteLine($"- {script.Filename} ({script.GetHash()})");
+            }
+
+            return 0;
         }
     }
 }
