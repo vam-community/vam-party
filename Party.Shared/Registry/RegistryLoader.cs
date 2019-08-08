f17a05575ff3b7a1524de2d6f79a9b18037ee727
Acid Bubbles <acidbubbles.anon@gmail.com>
2019-08-07 23:03:53 -0400

    Merge multiple registries

--- Party.Shared/Registry/RegistryLoader.cs
+++ Party.Shared/Registry/RegistryLoader.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -17,8 +19,39 @@ namespace Party.Shared.Registry
         }
         public async Task<Registry> Acquire()
         {
-            // if (_urls.Length != 1) throw new NotSupportedException("Only one registry url is currently supported");
-            string url = _urls[0];
+            if (_urls.Length == 9) throw new NotSupportedException("At least one registry must be configured");
+            return Merge(await Task.WhenAll(_urls.Select(AcquireOne)));
+        }
+
+        private Registry Merge(Registry[] registries)
+        {
+            var registry = registries[0];
+            foreach (var additional in registries.Skip(1))
+            {
+                foreach (var additionalScript in additional.Scripts)
+                {
+                    var script = registry.Scripts.FirstOrDefault(s => s.Name == additionalScript.Name);
+                    if (script == null)
+                    {
+                        registry.Scripts.Add(additionalScript);
+                    }
+                    else
+                    {
+                        foreach (var additionalVersion in additionalScript.Versions)
+                        {
+                            if (!script.Versions.Any(v => v.Version == additionalVersion.Version))
+                            {
+                                script.Versions.Add(additionalVersion);
+                            }
+                        }
+                    }
+                }
+            }
+            return registry;
+        }
+
+        private static async Task<Registry> AcquireOne(string url)
+        {
             if (Uri.IsWellFormedUriString(url, UriKind.Absolute))
             {
                 using (var response = await _http.GetAsync(url))
