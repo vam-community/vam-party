fa5c8fda706d92cca79168b61053643a3ca12bea
Acid Bubbles <acidbubbles.anon@gmail.com>
2019-09-04 22:40:58 -0400

    Add a test for show

--- Party.CLI.Tests/Commands/ShowTests.cs
+++ Party.CLI.Tests/Commands/ShowTests.cs
@@ -11,7 +11,7 @@ namespace Party.CLI
     {
         [Test]
         [SetUICulture("en-US")]
-        public async Task Show()
+        public async Task ShowWithMinimalInfo()
         {
             _controller.Setup(x => x.HealthCheck());
             DateTimeOffset created = new DateTimeOffset(2010, 11, 12, 0, 0, 0, 0, TimeSpan.Zero);
@@ -33,6 +33,67 @@ namespace Party.CLI
                                 {
                                     new RegistryFile
                                     {
+                                        Filename = "File 1.cs"
+                                    }
+                                }
+                            }
+                        })
+                    }
+                })
+            });
+            _controller.Setup(x => x.GetSavesAsync(null)).ReturnsAsync(new SavesMap());
+
+            var result = await _program.Execute(new[] { "show", "cool-thing" });
+
+            Assert.That(GetOutput(), Is.EqualTo(new[]{
+                "Package cool-thing",
+                $"Last version v1.2.3, published {created.ToLocalTime().ToString("D")}",
+                "Versions:",
+                $"- v1.2.3, published {created.ToLocalTime().ToString("D")}: (no release notes)",
+                "Author: some dude",
+                "Files:",
+                "- File 1.cs: not available in registry"
+            }));
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        [SetUICulture("en-US")]
+        public async Task ShowWithExtraInfo()
+        {
+            _controller.Setup(x => x.HealthCheck());
+            DateTimeOffset created = new DateTimeOffset(2010, 11, 12, 0, 0, 0, 0, TimeSpan.Zero);
+            _controller.Setup(x => x.GetRegistryAsync()).ReturnsAsync(new Registry
+            {
+                Authors = new SortedSet<RegistryAuthor>(new[]
+                {
+                    new RegistryAuthor{
+                        Name = "some dude",
+                        Github = "https://github.com/...profile",
+                        Reddit = "https://reddit.com/...profile"
+                    }
+                }),
+                Scripts = new SortedSet<RegistryScript>(new[]
+                {
+                    new RegistryScript
+                    {
+                        Name = "cool-thing",
+                        Author = "some dude",
+                        Tags = new List<string>(new[]{"tag1", "tag2"}),
+                        Description = "This does some things",
+                        Homepage = "https://reddit.com/...homepage",
+                        Repository = "https://github.com/...repo",
+                        Versions = new SortedSet<RegistryScriptVersion>(new []
+                        {
+                            new RegistryScriptVersion
+                            {
+                                Version = "1.2.3",
+                                Created = created,
+                                Notes = "Some cool new stuff",
+                                Files = new SortedSet<RegistryFile>
+                                {
+                                    new RegistryFile
+                                    {
                                         Filename = "File 1.cs",
                                         Url = "https://example.org/File%201.cs"
                                     }
@@ -49,7 +110,15 @@ namespace Party.CLI
             Assert.That(GetOutput(), Is.EqualTo(new[]{
                 "Package cool-thing",
                 $"Last version v1.2.3, published {created.ToLocalTime().ToString("D")}",
+                "Versions:",
+                $"- v1.2.3, published {created.ToLocalTime().ToString("D")}: Some cool new stuff",
+                "Description: This does some things",
+                "Tags: tag1, tag2",
+                "Repository: https://github.com/...repo",
+                "Homepage: https://reddit.com/...homepage",
                 "Author: some dude",
+                "- Github: https://github.com/...profile",
+                "- Reddit: https://reddit.com/...profile",
                 "Files:",
                 "- File 1.cs: https://example.org/File%201.cs"
             }));
