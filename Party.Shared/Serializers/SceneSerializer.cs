f5ecdc5a58c89539500f80d7736339860476775a
Acid Bubbles <acidbubbles.anon@gmail.com>
2019-09-19 10:30:43 -0400

    Constrain the fast scene scripts regex

--- Party.Shared/Serializers/SceneSerializer.cs
+++ Party.Shared/Serializers/SceneSerializer.cs
@@ -13,7 +13,7 @@ namespace Party.Shared.Serializers
     public class SceneSerializer : ISceneSerializer
     {
         private static readonly Regex _findScriptsFastRegex = new Regex(
-            "\"plugin#[0-9]+\" ?: ?\"(?<path>[^\"]+)\"",
+            "\"plugin#[0-9]{1,3}\" ?: ?\"(?<path>[^\"]{4,512})\"",
             RegexOptions.Compiled | RegexOptions.Multiline,
             TimeSpan.FromSeconds(5));
 
